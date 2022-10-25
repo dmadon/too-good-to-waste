ba36dafc62e673ebd43ed7628acf6a2c8d75ebdf
dmadon <deanna.madon@gmail.com>
2022-10-25 11:11:56 -0500

    .env secret works in auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.SECRET;
+const secret = `${process.env.SECRET}`;
 const expiration = '2h';
 
 module.exports = {
