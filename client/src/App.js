ea8e29fc43ff4356cad1bd05ed60a72ea4a85974
allibrdoine <allibrodine81@gmail.com>
2022-10-28 08:35:57 -0500

    restructure app.js and nav menu with react router, add home.js as page

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,51 @@
+import React from 'react';
 import './App.css';
-import PhotoCarousel from './components/Carousel/Carousel';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Box, Button, Text } from '@chakra-ui/react';
 import NavMenu from './components/NavMenu/NavMenu';
+import PartnerInventory from './pages/PartnerInventory';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import PartnerLogin from './pages/PartnerLogin';
+import CustomerPage from './pages/CustomerPage';
+
 
 function App() {
   document.title = 'Too Good To Waste';
 
   return (
     <div>
+      <Router>
       <ChakraProvider>
       <Box>
-      <NavMenu />
+        <NavMenu />
         
-        <Box display="flex" justifyContent="center">          
-          <h1 className="blk-let">TOO</h1>
-          <h1 className="csv-let">Good</h1>
-          <h1 className="blk-let">TO</h1>
-          <h1 className="csv-let">Waste</h1>   
-        </Box>
+        <Link id="home" to="/">
+          <Box display="flex" justifyContent="center">          
+              <h1 className="blk-let">TOO</h1>
+              <h1 className="csv-let">Good</h1>
+              <h1 className="blk-let">TO</h1>
+              <h1 className="csv-let">Waste</h1>   
+          </Box>
+        </Link>
 
-        <Box mt={7} mb={7}>       
-          <PhotoCarousel /> 
-        </Box>
+      </Box>      
 
-        <Text fontSize='3xl' textAlign={'center'} p={5}>
-          If 25% of the food currently being lost or wasted globally was saved, it would be enough to feed 870
-          million people around the world. Do your part to help reduce food waste by purchasing discounted grocery 
-          items that would have otherwise been thrown away. 
-        </Text>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/locator" element={<Locator />} /> */}
+          <Route path="/partnerlogin" element={<PartnerLogin />} />
+          <Route path="/education" element={<CustomerPage />} />
+        </Routes>
+      </main>
 
-        <Box display="flex" justifyContent="center" mt={10}>
-          <Button background='red' className='available'>See what's available near you!</Button>
-        </Box>
-
-        <Text fontSize='2xl' textAlign={'center'} mt={10}>*Subject to availability.</Text>
-        </Box>      
       </ChakraProvider>
+      </Router>
     </div>
   );
 }
--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { stack as Menu } from 'react-burger-menu';
 import './Nav.css'
+import { Link } from 'react-router-dom';
 
-const NavMenu = (props) => {
+const NavMenu = () => {
     return (
-        <Menu width={'175px'} {...props}>
-            <a id="signup" className="menu-item" href="/signup">Sign Up</a>
-            <a id="login" className="menu-item" href="/login">Customer Login</a>
-            <a id="education" className="menu-item" href="/education">Education</a>
-            <a id="provider" className="menu-item" href="/provider">Provider Login</a>
-            <a id="locator" className="menu-item" href="/locator">Store Locator</a>
+        <Menu width={'175px'} >
+            <Link id="signup" to="/signup" className="menu-item">Sign Up</Link>
+            <Link id="login" to="/login" className="menu-item">Customer Login</Link>
+            <Link id="education" to="/education" className="menu-item">Education</Link>
+            <Link id="partner" to="/partnerlogin" className="menu-item">Partner Login</Link>
+            <Link id="locator" to="/locator" className="menu-item">Store Locator</Link>
         </Menu>         
     );
 };
