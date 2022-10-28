7be2ac0ddd8610508548b8d16c503fcb05aa99bf
allibrdoine <allibrodine81@gmail.com>
2022-10-28 09:06:44 -0500

    hide nav menu after click

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { stack as Menu } from 'react-burger-menu';
 import './Nav.css'
 import { Link } from 'react-router-dom';
 
+
 const NavMenu = () => {
+    const [isOpen, setOpen] = useState(false);
+    
+    const handleIsOpen = () => {
+        setOpen(!isOpen)
+    }
+
+    const closeMenu = () => {
+        setOpen(false)
+    }
+
     return (
-        <Menu width={'175px'} >
-            <Link id="signup" to="/signup" className="menu-item">Sign Up</Link>
-            <Link id="login" to="/login" className="menu-item">Customer Login</Link>
-            <Link id="education" to="/education" className="menu-item">Education</Link>
-            <Link id="partner" to="/partnerlogin" className="menu-item">Partner Login</Link>
-            <Link id="locator" to="/locator" className="menu-item">Store Locator</Link>
-        </Menu>         
+        <Menu width={'175px'} isOpen={isOpen} onOpen={handleIsOpen} onClose={handleIsOpen}>
+            <Link id="signup" to="/signup" className="menu-item" onClick={closeMenu}>Sign Up</Link>
+            <Link id="login" to="/login" className="menu-item" onClick={closeMenu}>Customer Login</Link>
+            <Link id="education" to="/education" className="menu-item" onClick={closeMenu}>Education</Link>
+            <Link id="partner" to="/partnerlogin" className="menu-item" onClick={closeMenu}>Partner Login</Link>
+            <Link id="locator" to="/locator" className="menu-item" onClick={closeMenu}>Store Locator</Link>
+        </Menu>        
     );
 };
 
