7d6467d022122812d9674d3a6d30629c15869d15
allibrdoine <allibrodine81@gmail.com>
2022-10-30 11:56:04 -0500

    style added to education page, animation package installed

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import PartnerLogin from './pages/PartnerLogin';
+import Education from './pages/Education/Education';
 
 function App() {
   document.title = 'Too Good To Waste';
@@ -16,29 +17,29 @@ function App() {
     <div>
       <Router>
         <ChakraProvider>
-        <Box minH='1500px' bgColor='#F5EFE6'>
-          <header>
-            <NavMenu />
-                        
-            <Box display="flex" justifyContent="center">          
-              <Link id="home" to="/" className="blk-let">TOO</Link>
-              <Link id="home" to="/" className="csv-let">Good</Link>
-              <Link id="home" to="/" className="blk-let">TO</Link>
-              <Link id="home" to="/" className="csv-let">Waste</Link>   
-            </Box>        
-          </header>      
+          <Box minH='1500px' bgColor='#F5EFE6'>
+            <header>
+              <NavMenu />
+                          
+              <Box display="flex" justifyContent="center">          
+                <Link id="home" to="/" className="blk-let">TOO</Link>
+                <Link id="home" to="/" className="csv-let">Good</Link>
+                <Link id="home" to="/" className="blk-let">TO</Link>
+                <Link id="home" to="/" className="csv-let">Waste</Link>   
+              </Box>        
+            </header>      
 
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-              {/* <Route path="/locator" element={<Locator />} /> */}
-              <Route path="/partnerlogin" element={<PartnerLogin />} />
-              {/* <Route path="/education" element={<Education />} /> */}
-            </Routes>
-          </main>
-        </Box>
+            <main>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route path="/locator" element={<Locator />} /> */}
+                <Route path="/partnerlogin" element={<PartnerLogin />} />
+                <Route path="/education" element={<Education />} />
+              </Routes>
+            </main>
+          </Box>
         </ChakraProvider>
       </Router>
     </div>
