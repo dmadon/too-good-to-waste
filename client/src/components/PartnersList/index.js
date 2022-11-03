125f9d5f9e4a0424600bd46de8b039bea83d30d1
dmadon <deanna.madon@gmail.com>
2022-11-03 18:28:22 -0500

    pull new changes from develop

--- client/src/components/PartnersList/index.js
+++ client/src/components/PartnersList/index.js
@@ -3,29 +3,16 @@ import { Box, Text, Button } from '@chakra-ui/react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { SET_SELECTED_PARTNER, SET_SELECTED_INVENTORY} from '../../utils/actions';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_ALL_PARTNERS } from '../../utils/queries';
+// import { useQuery } from '@apollo/client';
+// import { QUERY_ALL_PARTNERS } from '../../utils/queries';
 import dayjs from 'dayjs';
 
-const PartnersList = ({ title }) => {
+const PartnersList = ({partners}) => {
   
   const [state, dispatch] = useStoreContext();
-  const {data, loading} = useQuery(QUERY_ALL_PARTNERS);
-  const [partners, setPartners] = useState([]);
+ 
   const {selectedInventory, today} = state;
 
-  const clearSelectedInventory = async () => {
-      if(selectedInventory){
-          await dispatch({
-              type:SET_SELECTED_INVENTORY,
-              inventoryData:{}
-          });
-      }
-  };
-
-  useEffect(() => {
-      clearSelectedInventory();
-  },[]);
 
   const handleSelectPartner = async (event) => {
       // the id attribute of the clicked button should be set to that partner's _id
@@ -38,24 +25,6 @@ const PartnersList = ({ title }) => {
       });
   }; 
 
-  console.log(`selected partner id: ${state.selectedPartner}`)
-
-  const getPartners = async () => {
-      try{
-          await data;
-          if(data){
-              setPartners(data.getPartners);
-          }
-      }catch(err){
-          console.log(err)
-      }
-  };
-
-  useEffect(() => {
-      getPartners();
-  },[data,loading,setPartners,partners])
-
-  console.log(partners)
   
   if (!partners.length) {
     return <h3>No stores available currently</h3>;
@@ -63,7 +32,7 @@ const PartnersList = ({ title }) => {
 
   return (
     <Box>
-      <Text fontFamily='Pacifico' fontSize='30px' color='#F5EFE6' textShadow='2px 2px #040303' fontWeight='normal' mb={3}>{title}</Text>
+      <Text fontFamily='Pacifico' fontSize='30px' color='#F5EFE6' textShadow='2px 2px #040303' fontWeight='normal' mb={3}>Participating Stores</Text>
       <Box>
         
         {partners && partners.map(partner => (
