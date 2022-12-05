c4c6356082998fdef443aa588f9a30477a97907e
allibrdoine <allibrodine81@gmail.com>
2022-12-05 13:48:15 -0600

    fade in transition added to modals

--- client/src/components/Modals/SignUpModal.js
+++ client/src/components/Modals/SignUpModal.js
@@ -4,12 +4,12 @@ import Login from '../../pages/Signup/Signup';
 import { Button } from '@chakra-ui/react';
 
 const Modal = props => {
-    if (!props.show) {
-        return null
-    };
+    // if (!props.show) {
+    //     return null
+    // };
 
     return (
-        <div className='modal' onClick={props.onClose}>
+        <div className={`modal ${props.show ? 'show' : ''}`} onClick={props.onClose}>
             <div className='modal-content' onClick={e => e.stopPropagation()}>
                 <Login />
                 <Button size='xs' className='close-btn' onClick={props.onClose}>Close</Button>
